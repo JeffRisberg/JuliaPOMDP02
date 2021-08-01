abda50f72d7889c475a73a4f4d195fbac2cc3bc5
JeffRisberg <j.s.risberg@gmail.com>
2021-08-01 14:43:08 -0700

    first commit

---
+++ test/hw1.jl
@@ -0,0 +1,23 @@
+#=
+hw1:
+- Julia version:
+- Author: jeff
+- Date: 2021-08-01
+=#
+@testset "HW1" begin
+    using DMUStudent.HW1
+    @test 1 <= fx([2]) <= 4
+    @test 1 <= fy([2]) <= 4
+    @test 1 <= fy([1, 2, 4]) <= 7
+
+    f(x, y) = sqrt(x^2 + y^2)
+    results = HW1.evaluate(f, "test1234@colorado.edu")
+    @test results.score == 1
+
+    results = HW1.evaluate(f)
+    @test results.score == 1
+
+    g(x, y) = x+y
+    results = HW1.evaluate(g, "test1234@colorado.edu")
+    @test results.score == 0
+end
